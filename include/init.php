6da2b1e0524c152f1505b394c4b5dbf64eef0a65
Temi <i.temi@wustl.edu>
2024-06-04 10:48:46 -0500

    added include files

---
+++ include/init.php
@@ -0,0 +1,14 @@
+<?php
+    date_default_timezone_set('America/Chicago');
+    session_start();
+
+    include('include/connect.php');
+		
+		// This should happen right after connect.php (config)
+		// so other functions have access to the database
+    include('include/db_query.php');
+		
+	
+
+    include('include/helper_functions.php');
+   
