e7fdc0e255d6f66ded67f48d2dba388de6dabf2e
Temi <i.temi@wustl.edu>
2024-06-04 16:01:13 -0500

    in progress navbar

---
+++ welcome_page.php
@@ -0,0 +1,20 @@
+<?php
+    include('include/init.php');
+?>
+    <html>
+        
+        <head>
+        <title>Temi</title>
+        <link rel='stylesheet' href='style(1).css'>
+
+        </head>
+        <body class = 'jost' style ='background-color:#020001'> 
+            <div class ='navbar'>
+                <ul class = ''>
+                    <li id = 'homeIconNavbarDiv'><a href ='welcome_page.php'><img src = 'images/welcome_page_home_icon.svg'></a></li>
+                    <li id='designerArtistStoryteller'><img src = 'images/designer_artist_storyteller.svg'></li>
+                    <li><img src = 'images/work.svg'></li>
+                    <li id='contactPosition'><img src = 'images/contact.svg'></li>
+                </ul>
+            </div>
+    </html>
