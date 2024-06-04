528259af1c6ea413024ab5d3b6700362c64a97e3
Temi <i.temi@wustl.edu>
2024-06-04 16:49:42 -0500

    pretty static welcome page

---
+++ loading_page.php
@@ -0,0 +1,15 @@
+<?php
+    include('include/init.php');
+?>
+    <html>
+        
+        <head>
+        <title>Welcome</title>
+        <link rel='stylesheet' href='style(1).css'>
+
+        </head>
+        <body style = 'background-color:#020001'> 
+            <img id='loadingIcon' src = 'images/loadingIcon.svg'>
+        </body>
+    </html>
+    
--- welcome_page.php
+++ welcome_page.php
@@ -4,17 +4,24 @@
     <html>
         
         <head>
-        <title>Temi</title>
+        <title>Welcome</title>
         <link rel='stylesheet' href='style(1).css'>
 
         </head>
-        <body class = 'jost' style ='background-color:#020001'> 
-            <div class ='navbar'>
+        <body style = 'background-color:#020001'> 
+        <!-- <body> -->
+            <div class = 'navbar'>
                 <ul class = ''>
-                    <li id = 'homeIconNavbarDiv'><a href ='welcome_page.php'><img src = 'images/welcome_page_home_icon.svg'></a></li>
+                    <li id = 'homeIconNavbarDiv'><a href = 'welcome_page.php'><img src = 'images/welcome_page_home_icon.svg'></a></li>
                     <li id='designerArtistStoryteller'><img src = 'images/designer_artist_storyteller.svg'></li>
                     <li><img src = 'images/work.svg'></li>
                     <li id='contactPosition'><img src = 'images/contact.svg'></li>
                 </ul>
             </div>
+            <div>
+                <img id = 'welcomePageName' src = 'images/TEMI.svg'>
+                <a href= 'loading_page.php'><img id = 'welcomePageIconClick' src = 'images/welcome_page_iconClick.svg'></a>
+            </div>
+            <img id = 'clickMe' src = 'images/click_me.svg'>
+
     </html>
