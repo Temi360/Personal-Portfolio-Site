13c62475b2c73253247ad91633cb370947d9b40d
Temi <i.temi@wustl.edu>
2024-06-07 16:11:10 -0500

    changes to header (made into text and not svg)

--- desk_page.php
+++ desk_page.php
@@ -7,6 +7,10 @@
         <title>Welcome</title>
         <link rel='stylesheet' href='style(1).css'>
         <link rel='stylesheet' href='desk_page.css'>
+        <link rel='stylesheet' href='style(1).css'>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 
         </head>
@@ -15,9 +19,9 @@
             <div class = 'navbar'>
                 <ul class = ''>
                     <li id = 'homeIconNavbarDiv'><a href = 'welcome_page.php'><img src = 'images/home_icon(desk_page).svg'></a></li>
-                    <li id='designerArtistStoryteller'><img src = 'images/designer_artist_storyteller(desk_page).svg'></li>
-                    <li><img src = 'images/work.svg'></li>
-                    <li id='contactPosition'><img src = 'images/contact.svg'></li>
+                    <li class = 'jost'><h3 style = 'color: #020001'>designer, artist, storyteller...</h3></li>
+                    <li class = 'jost'><h3 style = 'color: #9CAFFE'>work.</h3></li>
+                    <li class = 'jost'><h3 style = 'color: #9CAFFE'>contact.</h3></li>
                 </ul>
             </div>
     </html>
--- welcome_page.php
+++ welcome_page.php
@@ -6,6 +6,9 @@
         <head>
         <title>Welcome</title>
         <link rel='stylesheet' href='style(1).css'>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
         </head>
         <body style = 'background-color:#020001'> 
@@ -13,16 +16,16 @@
             <div class = 'navbar'>
                 <ul class = ''>
                     <li id = 'homeIconNavbarDiv'><a href = 'welcome_page.php'><img src = 'images/home_icon(welcome_page).svg'></a></li>
-                    <li id='designerArtistStoryteller'><img src = 'images/designer_artist_storyteller(welcome_page).svg'></li>
-                    <li><img src = 'images/work.svg'></li>
-                    <li id='contactPosition'><img src = 'images/contact.svg'></li>
+                    <li class = 'jost'><h3>designer, artist, storyteller...</h3></li>
+                    <li class = 'jost'><h3 style = 'color: #9CAFFE'>work.</h3></li>
+                    <li class = 'jost'><h3 style = 'color: #9CAFFE'>contact.</h3></li>
                 </ul>
             </div>
             <div class = 'WelcomePageNameAndIcon'>
                 <img id = 'welcomePageName' src = 'images/TEMI.svg'>
-                <div><a href = 'loading_page.php'><img id = 'welcomePageIconClick' src = 'images/welcome_page_iconClick.svg'></div></a>
+                <a href = 'loading_page.php'><img id = 'welcomePageIconClick' src = 'images/welcome_page_iconClick.svg'></a>
             </div>
-            <a href = 'loading_page.php'><img id = 'clickMe' src = 'images/click_me.svg'></a>
+            <img id = 'clickMe' src = 'images/click_me.svg'>
 
             
 
