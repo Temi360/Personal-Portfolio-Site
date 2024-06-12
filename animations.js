807464b45f685ac3b1463b7a132051b03b4f3ae8
Temi <i.temi@wustl.edu>
2024-06-12 17:01:36 -0500

    animationfrenzy

--- animations.js
+++ animations.js
@@ -4,5 +4,12 @@ function lightOn(){
 }
 
 function fanOn(){
- alert("function call");
+//  alert("function call");
+    var blades = document.getElementById("blades");
+    blades.classList.toggle("blades-rotate");
+    var hair = document.getElementById("hair-bottom");
+    hair.classList.toggle("hair-blows");
+    var postIt = document.getElementById("postIt");
+    postIt.classList.toggle("postIt-blows");
+
 }
