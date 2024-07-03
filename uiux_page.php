d80e01574e3617888f6b4df1a80e5ed94914d1b3
Temi <i.temi@wustl.edu>
2024-07-03 13:48:31 -0500

    addded portfolio pages

---
+++ uiux_page.php
@@ -0,0 +1,62 @@
+
+<html>
+        
+        <head>
+ 
+        <title>UI/UX</title>
+            <link rel="stylesheet" href = "general.css">
+            <link rel="stylesheet" href = "portfolio_page.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+             <link href="https://fonts.googleapis.com/css2?family=Just+Me+Again+Down+Here&display=swap" rel="stylesheet">
+            <script src="JS/gsap.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/Draggable.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/MotionPathPlugin.min.js"></script>
+            <script src="JS/animations.js"></script>
+            <script src="JS/portfolio_page.js"></script>
+        </head>
+         <body> 
+         <div class = "navbar">
+                     <ul>
+                         <li class = "jost" alt = "home icon" id = "homeIconNavbarDiv"><span onclick="transitionToPage('welcome_page.php')"><img alt = "home icon" src = "images/home_icon.svg"></span></li>
+                         <li class = "jost" style = "color: var(--black)">designer, coder, storyteller<span style = "color: var(--orange)">...</span></li>
+                         <li style = "color: var(--orange);" class = "jost">work.</span></li>
+                         <li class = "jost">contact<span style = "color: var(--orange)">.</span></li>
+                     </ul>
+                 </div>
+                
+             <div class = "folderBackgroundDiv">
+             <div class = "folderPageTitle"> UI/UX</div>
+                <div class = "menuAndBackButton">
+                <div style = "color: var(--darkDarkGrey)" id = "folderMenu"> Home > Work > Portfolio > <span style = "color: var(--black)"> UI/UX </span></div>
+            </div>
+
+            <div class = "portfolioContentPreview">
+            <img src = "images/leftChevron.svg">
+            <img src = "images/theWindyScenePreview.svg">
+            <div class = "portfolioPreviewText jost ">
+            <h2> The Windy Scene</h2>
+            <p> A tour application that utilizes an easy userflow to make exploring Chicago's jazz scene simple and fun!</p>
+            </div>
+            <img src = "images/rightChevron.svg">
+
+            </div>
+             <svg width="1133" height="550" viewBox="0 0 1133 550" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="folderBackground">
+<path id="Rectangle 17" d="M2.49996 63C2.49996 53.335 10.335 45.5 20 45.5H1113C1122.66 45.5 1130.5 53.335 1130.5 63V530C1130.5 539.665 1122.66 547.5 1113 547.5H20C10.335 547.5 2.5 539.665 2.5 530L2.49996 63Z" stroke="#9CAFFE" stroke-width="5"/>
+<path id="Vector 270" d="M37.7732 1.54008L12.9844 46.0401C11.8705 48.0397 13.3162 50.5 15.6052 50.5H215.082C217.327 50.5 218.777 48.1236 217.749 46.127L194.843 1.627C194.329 0.627929 193.3 0 192.176 0H40.394C39.3058 0 38.3028 0.589357 37.7732 1.54008Z" fill="#9CAFFE"/>
+<path id="exitButton" d="M175 15L191 31M191 15L175 31" stroke="#F5F5F5" stroke-width="3"/>
+<path id="Vector 272" d="M1123 109H10.077C8.42018 109 7.07704 107.657 7.07703 106L7.07693 62.1157C6.33534 56.8743 11.1297 51.6619 13.9513 49.8659C14.3463 49.6145 14.7896 49.5121 15.2579 49.5114C377.314 49.007 1102.64 48 1113.02 48C1123 49.2099 1125.83 56.2342 1126 59.595V106C1126 107.657 1124.66 109 1123 109Z" fill="#9CAFFE" stroke="#F5F5F5" stroke-width="3"/>
+<a href = "portfolio_page.php"><g id="backButton">
+<path id="backArrow" d="M27.2929 78.2929C26.9024 78.6834 26.9024 79.3166 27.2929 79.7071L33.6569 86.0711C34.0474 86.4616 34.6805 86.4616 35.0711 86.0711C35.4616 85.6805 35.4616 85.0474 35.0711 84.6569L29.4142 79L35.0711 73.3431C35.4616 72.9526 35.4616 72.3195 35.0711 71.9289C34.6805 71.5384 34.0474 71.5384 33.6569 71.9289L27.2929 78.2929ZM54 78H28V80H54V78Z" fill="#F5F5F5"/>
+<path id="back" d="M65.81 69.75C66.85 69.75 67.52 71.79 67.82 75.87L68 76.8C68.92 74.92 69.67 73.98 70.25 73.98C70.25 73.9 70.46 73.84 70.88 73.8H71C72.32 73.8 73.16 74.83 73.52 76.89V76.98C73.52 78.74 72.64 80.68 70.88 82.8C70.14 83.6 69.33 84 68.45 84C67.45 83.78 66.95 83.28 66.95 82.5L65.45 71.73L65 70.62C65 70.26 65.27 69.97 65.81 69.75ZM68.93 81.93H69.02C70.06 81.51 70.97 80.08 71.75 77.64V76.5C71.57 75.88 71.34 75.57 71.06 75.57H70.82C69.94 76.09 69.31 78.21 68.93 81.93ZM80.6429 72.42C81.5229 72.42 81.9629 72.77 81.9629 73.47L81.6929 74.04L81.5129 79.53C81.5129 82.01 82.0129 83.55 83.0129 84.15L83.0729 84.6C83.0129 85.18 82.7429 85.47 82.2629 85.47H82.0829C80.5829 84.45 79.8329 82.89 79.8329 80.79H79.6229C78.8229 83.03 77.8529 84.15 76.7129 84.15C75.6529 83.85 75.1229 83.06 75.1229 81.78C75.1229 78.96 76.2529 76.15 78.5129 73.35C79.2729 72.73 79.9829 72.42 80.6429 72.42ZM77.0129 80.58L76.8929 81.42V81.99H77.1929C78.6929 78.25 79.4429 75.97 79.4429 75.15H79.3229C78.1629 76.45 77.3929 78.26 77.0129 80.58ZM86.8527 73.83L88.0827 73.65C88.5827 73.65 88.8627 73.96 88.9227 74.58C88.9227 75.12 88.2727 75.47 86.9727 75.63C86.1527 76.27 85.7427 77.09 85.7427 78.09V79.08C86.1427 81.28 86.8227 82.38 87.7827 82.38H88.0827C88.6427 82.38 89.4827 81.85 90.6027 80.79H90.7827C91.1827 80.79 91.5027 81.07 91.7427 81.63C91.4627 82.49 90.5627 83.27 89.0427 83.97C88.7627 84.09 88.4127 84.15 87.9927 84.15C85.9327 84.15 84.5927 82.53 83.9727 79.29V78.63C83.9727 76.29 84.9327 74.69 86.8527 73.83ZM94.7855 68.13H94.9655C95.2255 68.13 95.4755 68.35 95.7155 68.79L95.5955 70.83V71.97L95.7155 77.22L95.8055 77.52H95.8955C96.7555 76.72 97.5055 75.31 98.1455 73.29L98.8055 73.08C99.3655 73.16 99.6455 73.41 99.6455 73.83C99.6455 75.13 98.7155 76.88 96.8555 79.08V79.29C99.3155 82.15 100.936 83.58 101.716 83.58H101.896L102.646 83.13H102.916C103.336 83.13 103.646 83.41 103.846 83.97C103.846 84.61 103.136 85.05 101.716 85.29C100.296 85.29 98.4455 83.85 96.1655 80.97H95.8955L95.9855 82.59V84.15C95.8655 84.77 95.5555 85.08 95.0555 85.08C94.4955 84.96 94.2155 84.65 94.2155 84.15V83.97L94.0955 80.52C93.8755 80.52 93.7255 80.21 93.6455 79.59L94.0355 78.84L93.8555 72.09V70.77C93.8555 69.01 94.1655 68.13 94.7855 68.13Z" fill="#F5F5F5"/>
+</g></a>
+</g>
+</svg>
+</div>
+</body>
+</html>
