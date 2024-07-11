41743a80a4ade752d2bb7e585998bfc2ebcc481d
Temi <i.temi@wustl.edu>
2024-07-11 15:20:40 -0500

    component work and routing

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-
+import { Route, Routes, Link } from 'react-router-dom';
+import Home from './WelcomePage';
+import ComputerPage from './ComputerPage';
+import ContactPage from './ContactPage';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
 
   return (
-
-    <Router>
+    <div>
       <nav>
         <ul>
-        <li>Home</li>
+        <li><Link to = "/">Home</Link></li>
         <li>designer, coder, storyteller...</li>
-        <li>work.</li>
-        <li>contact.</li>
+        <li><Link to = "/">work</Link></li>
+        <li><Link to = "/">contact</Link></li>
 
         </ul>
       </nav>
       <Routes>
-
+      <Route path="/" element={<Home />} />
+        <Route path="/work" element={<ComputerPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
-      </Router>
+      </div>
   );
 };
 
---
+++ react/src/ComputerPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function ComputerPage() {
+    return (
+         <>
+            <div>
+                This will be where the contact module
+            </div>
+         </>
+    )
+}
+
+export default ComputerPage
