e3def5f206e06847923f74f269427c2ee10868c9
Temi <i.temi@wustl.edu>
2024-07-25 13:29:21 -0500

    project page changes

---
+++ react/src/ProjectTemplate.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from "react";
+import "./portfolio_page.css";
+import greyLine from "./greyLine.svg";
+import windyScenePreview from "./theWindyScenePreview.svg";
+import theProblem from "./theProblem.svg";
+import figma from "./figmaIcon.svg";
+import plant from "./plant.svg";
+import infoArchitecture from "./TWSInfoArchitecture.svg";
+import loFiWireframeLabelAndPen from "./LoFiWireframeLabelAndPen.svg";
+import loFiWireframes from "./loFiWireframes.svg";
+import uxTestingLabel from "./uxLabel.svg";
+import lightBulb from "./lightBulb.svg";
+import finalPrototype from "./finalPrototypeLabel.svg";
+
+function ProjectTemplate({
+  title,
+  projectDescription,
+  ideation,
+  sketches,
+  revision,
+  finalProduct,
+  finalNotes,
+}) {
+  return (
+    <>
+      <div className="projectTemplate"></div>
+    </>
+  );
+}
