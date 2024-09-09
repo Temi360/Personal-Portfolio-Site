4d4a9bc42010fa991374910376a54c67ea06dab3
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-09-09 21:14:34 +0100

    fix: fixed projectData page

--- src/components/projectData.jsx
+++ src/components/projectData.jsx
@@ -1,26 +1,23 @@
-import theWindySceneImage from "./theWindyScenePreview.svg";
-import AnylyticsImage from "./anylyticsPreview.svg";
-import figmaIcon from "./figmaIcon.svg";
 const projectData = [
   {
     id: 1,
     title: "Anylytics.ai",
-    projectImage: AnylyticsImage,
+    projectImage: "/assets/anylyticsPreview.svg",
     type: "UI/UX",
     role: "Lead Designer",
     duration: "2 months",
-    stackImages: figmaIcon,
+    stackImages: "/assets/figmaIcon.svg",
     projectDescription:
       "AnylyticsAI, a startup specializing in AI-driven data insights, needed a sleek, user-friendly interface for their Data Copilot feature. I designed an intuitive yet robust user experience that enables seamless navigation between data scopes, tracks past conversations, and facilitates team collaboration on insights.",
   },
   {
     id: 2,
     title: "The Windy Scene",
-    projectImage: theWindySceneImage,
+    projectImage: "/assets/theWindyScenePreview.svg",
     type: "UI/UX",
     role: "Lead Designer",
     duration: "3 months",
-    stackImages: figmaIcon,
+    stackImages: "/assets/figmaIcon.svg",
     projectDescription:
       "There is currently no interactive user guide to the eclectic and historical jazz scene in one of the richest cities in the US. I mean, I had to do something about it. As a Chicago native, I valued the artistic identity of the city, and wanted to integrate that into the visual design and features of the application. That's how The Windy Scene was born!",
   },
